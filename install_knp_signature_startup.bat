b76fd46d1d1d92eb0e2ba241705f48d6aab63141
shubhamsinghrv <shubhamsinghvr@gmail.com>
2026-05-16 19:09:16 +0530

    vbs

--- install_knp_signature_startup.bat
+++ install_knp_signature_startup.bat
@@ -3,6 +3,8 @@ setlocal
 cd /d "%~dp0"
 
 set "STARTUP_DIR=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+set "TARGET_BAT=%~dp0start_knp_signature_windows.bat"
+set "STARTUP_VBS=%STARTUP_DIR%\KNP Signature.vbs"
 
 if not exist "%STARTUP_DIR%" (
   echo Startup folder not found.
@@ -10,7 +12,15 @@ if not exist "%STARTUP_DIR%" (
   exit /b 1
 )
 
-copy /Y "%~dp0start_knp_signature_hidden.vbs" "%STARTUP_DIR%\KNP Signature.vbs" >nul
+if not exist "%TARGET_BAT%" (
+  echo start_knp_signature_windows.bat was not found in:
+  echo %~dp0
+  pause
+  exit /b 1
+)
+
+> "%STARTUP_VBS%" echo Set shell = CreateObject("WScript.Shell")
+>> "%STARTUP_VBS%" echo shell.Run Chr(34^) ^& "%TARGET_BAT%" ^& Chr(34^), 0, False
 
 echo.
 echo KNP Signature auto-start has been installed.
